d517c9b1c4a3b291866020e0ac5dedcf66434c80
Katerina <smilevskakaterina5@gmail.com>
2024-07-05 14:55:01 +0200

    Connect to backend application.

---
+++ speaker-diarization-frontend/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import SpeakersComponent from '../Speakers/speakers';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SpeakersComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
